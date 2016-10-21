e4e4bbd0d75e83eef1dfd710eff7676a87edfd9b
missionkao <kaominghsuan@gmail.com>
2016-10-21 19:33:07 +0800

    WIP

---
+++ iCHEFMap/MapViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewModel.swift
+//  iCHEFMap
+//
+//  Created by Mission on 2016/10/21.
+//  Copyright © 2016年 ichef. All rights reserved.
+//
+
+import RealmSwift
+
+class MapViewModel: NSObject {
+    
+    //MARK: - Life Cycle
+    override init() {
+        super.init()
+    }
+    
+    deinit {
+        
+    }
+    
+}
--- iCHEFMap/ViewController.swift
+++ iCHEFMap/ViewController.swift
@@ -8,19 +8,46 @@
 
 import UIKit
 import GoogleMaps
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: GMSMapView!
+    
+    private var currentLocation: CLLocation?
+    private let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.locationManager.delegate = self
+        self.locationManager.requestWhenInUseAuthorization()
+//        let marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+//        marker.title = "Sydney"
+//        marker.snippet = "Australia"
+//        marker.map = mapView
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            self.currentLocation = location
+            self.mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+            self.locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.authorizedWhenInUse {
+            self.locationManager.startUpdatingLocation()
+            self.mapView.isMyLocationEnabled = true
+            self.mapView.settings.myLocationButton = true
+        }
     }
 
 }
